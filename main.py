25f2fbf1eb4b8488faabf66bca518151b27cf53d
Onting <biuebox@tutamail.com>
2023-07-15 17:49:58 +0100

    update

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import csv
+import configparser
 import pyautogui as pyg
 import pygame
 import tkinter as tk
-from TkinterDnD2 import DND_FILES, TkinterDnD
+import subprocess
+import webbrowser
 
-def load_data(filename):
+from tkinter import filedialog, messagebox, ttk
+
+class App:
+    def __init__(self):
+        # window setup
+        self.window = tk.Tk()
+        self.window.title("Auto Loom")
+        self.window.geometry("800x500")
+        self.window.pack_propagate(False)
+        self.window.resizable(0, 0)
+
+        self.audiofile_frame = tk.LabelFrame(self.window, text="Audio File")
+        self.audiofile_frame.place(height=100, width=400, relx=0.5, rely=0)
+
+        self.csvfile_frame = tk.LabelFrame(self.window, text="CSV File")
+        self.csvfile_frame.place(height=100, width=400, relx=0.001, rely=0)
+
+        self.load_cvs_button = tk.Button()
+
+        self.window.mainloop()
+
+
+def load_data(filename: str):
     """Loads CSV data into a list"""
     list_data = []
     with open(filename) as loom:
@@ -15,7 +39,7 @@ def load_data(filename):
         return list_data
 
 # need to test!!!
-def play_audio(file_path):
+def play_audio(file_path: str):
     pygame.mixer.init()
     pygame.mixer_music.load(file_path)
     pygame.mixer.music.play()
@@ -25,32 +49,40 @@ def play_audio(file_path):
 
     pygame.mixer.music.stop()
 
+def open_tab(url: str, path: str):
+    webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(path))
+    webbrowser.get('chrome').open_new(url)
+    
 
-def dnd_cvs_path(event):
-    pass
+def close_tab():
+    pyg.hotkey('ctrl', 'w')
+
 
 
 
 
 ld = load_data('Sheet1.csv')
 
+parser = configparser.ConfigParser()
+parser.read('config.ini')
+chrome_path = parser.get('path', 'chrome_path')
+loom_path = parser.get('path', 'chrome_path')
+
+print(parser.get('path', 'chrome_path'))
+
+subprocess.Popen()
+
 for cols in ld:
-    website = cols[1]
+    url = cols[1]
     owner_name = cols [2]
 
-    print(f"Website: {website}, Owner: {owner_name}")
+    # open_tab(url, chrome_path)
 
-# window = TkinterDnD.Tk()
-# window.geometry("800x500")
-# window.title("Auto Loomer")
+    # close_tab()
 
 
-
-
-
-
-
-# window.mainloop()
+if __name__ == "__main__":
+    app=App()
 
 
 # res = pyg.locateCenterOnScreen("recording.png")
